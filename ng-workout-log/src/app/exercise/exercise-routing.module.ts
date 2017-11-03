c658a4326960f7fd7de7431de115d2af65df6554
Chris Luo <chrisluo@debian.attlocal.net>
2017-11-02 21:08:23 -0700

    Added exercise detail path via id.

--- ng-workout-log/src/app/exercise/exercise-routing.module.ts
+++ ng-workout-log/src/app/exercise/exercise-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { ExerciseComponent } from "./exercise.component";
 import { Routes, RouterModule } from "@angular/router";
+import { ExerciseDetailComponent } from "./exercise-detail/exercise-detail.component";
 
 const exerciseRoutes: Routes = [
     {
         path: '',
         component: ExerciseComponent
     },
+    {
+        path: ':id',
+        component: ExerciseDetailComponent
+    }
 ]
 
 @NgModule({
