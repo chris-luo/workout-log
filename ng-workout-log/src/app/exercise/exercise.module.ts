c0256eb8d50ac69334be599c5dc2f9628a30e813
Chris Luo <chrisluo@debian.attlocal.net>
2017-11-02 21:09:27 -0700

    Declare new components.

--- ng-workout-log/src/app/exercise/exercise.module.ts
+++ ng-workout-log/src/app/exercise/exercise.module.ts
@@ -4,11 +4,15 @@ import { ExerciseComponent } from "./exercise.component";
 import { ExerciseListComponent } from "./exercise-list/exercise-list.component";
 import { SharedModule } from "../shared/shared.module";
 import { CommonModule } from "@angular/common";
+import { ExerciseListItemComponent } from './exercise-list/exercise-list-item/exercise-list-item.component';
+import { ExerciseDetailComponent } from './exercise-detail/exercise-detail.component';
 
 @NgModule({
     declarations: [
         ExerciseComponent,
-        ExerciseListComponent
+        ExerciseListComponent,
+        ExerciseListItemComponent,
+        ExerciseDetailComponent
     ],
     imports: [
         CommonModule,
