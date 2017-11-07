4a285971510344014e86317b9475a26fce579579
Chris Luo <chris.ying.luo@gmail.com>
2017-11-06 20:47:14 -0800

    Added LogEntry class and Set interface.

--- ng-workout-log/src/app/exercise/exercise.model.ts
+++ ng-workout-log/src/app/exercise/exercise.model.ts
@@ -1,3 +1,21 @@
 export class Exercise {
-    constructor(public name: string, public description: string, public imagePath: string) {}
+    constructor(
+        public name: string, 
+        public description: string, 
+        public imagePath: string
+    ) {}
+}
+
+export class LogEntry {
+    constructor(
+        public exercise: string,
+        public dateTime: string,
+        public sets: { weight: number, reps: number}[]
+    ) {}
+}
+
+export interface Set {
+    weight: number,
+    reps: number,
+    dateTime: string
 }
