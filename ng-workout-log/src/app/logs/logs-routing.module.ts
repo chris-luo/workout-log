0c86a3273c8f6bb22d7c3b528e4773a32d20a919
Chris Luo <chris.ying.luo@gmail.com>
2017-11-06 21:43:43 -0800

    Added logs-list component.

--- ng-workout-log/src/app/logs/logs-routing.module.ts
+++ ng-workout-log/src/app/logs/logs-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LogsComponent } from "./logs.component";
+import { LogsListComponent } from "./logs-list/logs-list.component";
 
 const LogRoutes: Routes = [
     {
         path: '',
         component: LogsComponent,
+        children: [
+            {
+                path: '',
+                component: LogsListComponent
+            }
+        ]
     }
 ]
 
--- ng-workout-log/src/app/logs/logs.module.ts
+++ ng-workout-log/src/app/logs/logs.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LogsRoutingModule } from './logs-routing.module';
 import { LogsComponent } from './logs.component';
 import { LogItemComponent } from './log-item/log-item.component';
+import { LogsListComponent } from './logs-list/logs-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LogsRoutingModule
   ],
-  declarations: [LogsComponent, LogItemComponent]
+  declarations: [LogsComponent, LogItemComponent, LogsListComponent]
 })
 export class LogsModule { }
