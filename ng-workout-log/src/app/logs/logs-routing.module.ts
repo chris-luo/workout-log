4d10f7b6235e0ed07d77e9aeafb4173c50a1b887
Chris Luo <chris.ying.luo@gmail.com>
2017-11-06 21:02:11 -0800

    Added Logs routing.

---
+++ ng-workout-log/src/app/logs/logs-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LogsComponent } from "./logs.component";
+
+const LogRoutes: Routes = [
+    {
+        path: '',
+        component: LogsComponent
+    }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(LogRoutes)
+    ],
+    exports: [
+        
+    ]
+})
+
+export class LogsRoutingModule {}
