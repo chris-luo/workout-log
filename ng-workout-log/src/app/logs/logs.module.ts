c553d8579ca01620fb2b819b8d49721bfb33d542
Chris Luo <chris.ying.luo@gmail.com>
2017-11-06 21:40:49 -0800

    Added log-item component.

--- ng-workout-log/src/app/logs/logs.module.ts
+++ ng-workout-log/src/app/logs/logs.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogsRoutingModule } from './logs-routing.module';
 import { LogsComponent } from './logs.component';
+import { LogItemComponent } from './log-item/log-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LogsRoutingModule
   ],
-  declarations: [LogsComponent]
+  declarations: [LogsComponent, LogItemComponent]
 })
 export class LogsModule { }
