fc4238df581120608eca9b1e194237fc994bd9e6
Chris Luo <chris.ying.luo@gmail.com>
2017-11-06 20:52:05 -0800

    Added logs component.

--- ng-workout-log/src/app/app.module.ts
+++ ng-workout-log/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ExerciseService } from './exercise/exercise.service';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
