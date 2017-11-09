ebfb5e210a1f98b2bdc40176f11afb59b8153eca
Chris Luo <chris.ying.luo@gmail.com>
2017-11-08 22:54:13 -0800

    node: Clean up.

--- node-workout-log/src/App.ts
+++ node-workout-log/src/App.ts
@@ -3,7 +3,6 @@ import * as config from "config";
 import * as http from "http";
 import * as logger from "morgan";
 import * as cors from "cors";
-import * as pg from "pg";
 
 import ResourceRouter from "./routes/ResourceRouter";
 
@@ -13,25 +12,6 @@ export class App {
     private port: number;
 
     constructor() {
-        const client = new pg.Client({
-            user: config.get('DB_USER'),
-            host: config.get('DB_HOST'),
-            database: config.get('DB_DATABASE'),
-            password: config.get('DB_PASSWORD'),
-            port: config.get('DB_PORT'),
-        });
-        client.connect();
-        client.query("SELECT * FROM person", (err, res) => {
-            if (!err) {
-                console.log(res.rows);
-            }
-            else {
-                console.log(err);
-            }
-            client.end()
-          });
-
-
         this.express = express();
         this.port = +config.get('PORT');
         this.createServer();
